1aa8770a4c8f062c3ecb1ef1d2d0b1f43f0cad8e
Elber Funez <EFunez@ecslimited.com>
2024-07-18 15:29:47 -0400

    add a return to get the players from the player service

--- src/app/components/questionbank/questionbank.component.ts
+++ src/app/components/questionbank/questionbank.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlayerService } from 'src/app/services/player.service';
 
 @Component({
   selector: 'app-questionbank',
   templateUrl: './questionbank.component.html',
   styleUrls: ['./questionbank.component.css']
 })
-export class QuestionbankComponent {
+export class QuestionbankComponent implements OnInit {
+  constructor(private playerService: PlayerService){ }
+  ngOnInit(): void {    
+  }
 
 }
